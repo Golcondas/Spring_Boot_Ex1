24eba2ce0c78fe8baccbcf598b94e1d932671ec1
Golcondas <754931220@qq.com>
2017-12-05 23:55:49 +0800

    #add Spring Boot中email的使用,但是发送邮件失败

--- src/main/java/com/neil/spboot/DemoApplication.java
+++ src/main/java/com/neil/spboot/DemoApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @SpringBootApplication
-@MapperScan("com.neil.spboot.mybatis.mapper")
+@EnableScheduling
+@MapperScan("com.neil.spboot.0mybatis.mapper")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
